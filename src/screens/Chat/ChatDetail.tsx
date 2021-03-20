8e1a6cc9a320362318ad2603cc526d7a3cbf322c
dattd <trandoandat38@gmail.com>
2021-03-20 22:44:16 +0700

    fix UI chatDetail

--- src/screens/Chat/ChatDetail.tsx
+++ src/screens/Chat/ChatDetail.tsx
@@ -1,18 +1,19 @@
-import React , {useState ,useEffect ,useCallback}from "react";
-import { View, ScrollView,Text, SafeAreaView, StyleSheet ,Platform} from "react-native";
-import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState, useEffect, useCallback } from "react";
+import { View, ScrollView, Image, SafeAreaView, StyleSheet, Platform } from "react-native";
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import styles from "./Styles";
+import { back } from "../../assets";
 import Colors from "../../utils/colors"
 import Header from "../../components/Header"
 import { IBaseProps } from "../../utils/interface";
-import { ic_back } from "../../assets";
+import TouchableComponent from "../../components/Button"
 
 const ChatDetail: React.FC<IBaseProps> = (props) => {
-  const {route ,navigation} = props
-  const {item} = route?.params
+  const { route, navigation } = props
+  const { item } = route?.params
   const [messages, setMessages] = useState<Array<any>>();
 
   useEffect(() => {
@@ -39,21 +40,23 @@ const ChatDetail: React.FC<IBaseProps> = (props) => {
       },
     ]);
   }, []);
-  const _onBack = useCallback(()=>{
-    navigation.pop()
-  },[navigation])
-    const onSend = useCallback((messages = []) => {
+
+  const _onBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const onSend = useCallback((messages = []) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages),
     );
   }, []);
-    const renderSend = (props) => {
+  const renderSend = (props) => {
     return (
       <Send {...props}>
         <View>
           <MaterialCommunityIcons
             name="send-circle"
-            style={{marginBottom: 5, marginRight: 5}}
+            style={{ marginBottom: 5, marginRight: 5 }}
             size={32}
             color="#2e64e5"
           />
@@ -78,8 +81,8 @@ const ChatDetail: React.FC<IBaseProps> = (props) => {
       />
     );
   };
-   const scrollToBottomComponent = () => {
-    return(
+  const scrollToBottomComponent = () => {
+    return (
       <FontAwesome name='angle-double-down' size={22} color='#333' />
     );
   }
@@ -87,26 +90,34 @@ const ChatDetail: React.FC<IBaseProps> = (props) => {
   return (
     <View style={styles.container}>
       <Header
-        btnIconLeft={ic_back}
-        onBack={_onBack}
-        backgroundColor={Colors.BLUE_OPACITY}
+        backgroundColor={Colors.white}
+        headerLeft={
+          <TouchableComponent onPress={_onBack}>
+            <Image source={back} style={{ width: 30, height: 15 }} />
+          </TouchableComponent>
+        }
+        titleStyle={{
+          fontSize: 17,
+          fontWeight: "bold",
+          color: Colors.black,
+        }}
         title={item?.userName}
       />
-     <View style={{flex:1}}>
+      <View style={{ flex: 1 }}>
         <GiftedChat
-      messages={messages}
-      onSend={(messages) => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-      renderBubble={renderBubble}
-      alwaysShowSend
-      renderSend={renderSend}
-      scrollToBottom
-      scrollToBottomComponent={scrollToBottomComponent}
+          messages={messages}
+          onSend={(messages) => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+          renderBubble={renderBubble}
+          alwaysShowSend
+          renderSend={renderSend}
+          scrollToBottom
+          scrollToBottomComponent={scrollToBottomComponent}
 
-    />
-     </View>
+        />
+      </View>
     </View>
   );
 };
